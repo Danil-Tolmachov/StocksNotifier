4adb343ee970be6e5ac37079a13af733bbbff9ac
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-06-16 15:20:31 +0300

    Update checkers.py & rename api_client.py

---
+++ api/services/api_client.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+from aiohttp import ClientSession
+from aiohttp.web_exceptions import HTTPNotFound
+import ujson
+
+from services.ticker import AbstractTiker
+import settings
+
+
+
+class AbstractAPIClient(ABC):
+    _instance = None
+    URL = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    @abstractmethod
+    def get_api_token() -> str:
+        pass
+
+    @abstractmethod
+    async def tiker_list(self) -> dict:
+        pass
+
+    @abstractmethod
+    async def tiker_details(self, tiker: str) -> dict:
+        pass
+
+
+class PolygonIoClient(AbstractAPIClient):
+    URL = 'https://api.polygon.io/v3/'
+    
+    def get_api_token(self) -> str:
+        return settings.POLYGONIO_API_KEY
+    
+
+    async def tiker_list(self, tiker: AbstractTiker, session: ClientSession):
+        query = f'?apiKey={self.get_api_token()}'
+        request = self.URL + 'reference/tickers/' + query
+
+        async with session.get(request) as response:
+
+            if response.status == 404:
+                raise HTTPNotFound()
+            
+            return ujson.loads(await response.text())
+        
+
+    async def tiker_details(self, tiker: AbstractTiker, session: ClientSession):
+        query = f'?apiKey={self.get_api_token()}'
+        request = self.URL + 'reference/tickers/' + tiker.tiker + query
+
+        async with session.get(request) as response:
+            
+            if response.status == 404:
+                raise HTTPNotFound()
+            
+            return ujson.loads(await response.text())
+
+
+# async def main():
+#     cl1 = PolygonIoClient()
+# 
+#     async with ClientSession() as session:
+#         print(await cl1.tiker_details(Tiker('AAPL'), session))
+# 
+# asyncio.run(main())
--- api/services/checkers.py
+++ api/services/checkers.py
@@ -1,104 +1,81 @@
 from abc import ABC, abstractmethod
-from aiohttp import ClientSession
-from aiohttp.web_exceptions import HTTPNotFound
-import settings
-import ujson
+from datetime import datetime, timedelta
+
+from services.ticker import AbstractTiker
 
 
+class DelayMixin():
+    min_delay: datetime = datetime(hour=10)
 
-class AbstractTiker(ABC):
-    def __init__(self, tiker) -> None:
-        super().__init__()
-        self.tiker = tiker
+    def __init__(self) -> None:
+        self.last_event: datetime = datetime.now()
 
-    def __str__(self) -> str:
-        return self.tiker
+    def _condition(self, tiker: AbstractTiker) -> bool:
+        if self.last_event + self.min_delay > datetime.now():
+            return False
 
-class Tiker(AbstractTiker):
-    pass
+class PriceRelatedMixin():
+    async def update_last_price(self):
+        self.last_price = await self.ticker.get_price()
 
 
 class AbstractChecker(ABC):
-    _instances = []
+    @classmethod
+    async def create(cls, tiker: AbstractTiker):
+        self = cls()
+        self.last_price = await tiker.get_price()
+        self.tiker = tiker
 
-    def __new__(cls, tiker: AbstractTiker):
+    def check(self) -> bool:
+        return self._condition()
 
-        for obj in cls._instances:
-            if obj.target.tiker == tiker: # Change to obj
-                return obj
+    @abstractmethod
+    def _condition(self, tiker: AbstractTiker) -> bool:
+        pass
 
-        new_obj = super().__new__(cls)
-        cls._instances.append(new_obj)
-        return new_obj
 
-    def __init__(self, target: AbstractTiker) -> None:
+
+class EverydayChecker(AbstractChecker):
+    delivery_time: datetime = datetime(hour=12)
+
+    def __init__(self) -> None:
         super().__init__()
-        self.target = target
+
+        self.delivery_date = None
+        self.update_delivery_date()
+
+    def update_delivery_date(self):
+        self.delivery_date = datetime.now() + timedelta(days=1)
+        self.delivery_date.replace(hour=self.delivery_time.hour, 
+                                   minute=self.delivery_time.minute)
+
+    async def _condition(self) -> bool:
+
+        if datetime.now() > self.delivery_date:
+            self.update_delivery_date()
+            return True
+
+        return False
 
 
-class BaseChecker(AbstractChecker):
-    pass
+class DropChecker(PriceRelatedMixin, DelayMixin, AbstractChecker):
 
+    async def _condition(self) -> bool:
+        current_price = await self.ticker.get_price()
 
+        if current_price > self.last_price:
+            await self.update_last_price()
+            return True
 
-class AbstractAPIClient(ABC):
-    _instance = None
-    URL = None
+        return False
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
+class GrowthChecker(PriceRelatedMixin, DelayMixin, AbstractChecker):
 
-        return cls._instance
+    async def _condition(self) -> bool:
+        current_price = await self.ticker.get_price()
 
-    @abstractmethod
-    def get_api_token() -> str:
-        pass
+        if current_price < self.last_price:
+            await self.update_last_price()
+            return True
 
-    @abstractmethod
-    async def tiker_list(self) -> dict:
-        pass
-
-    @abstractmethod
-    async def tiker_details(self, tiker: str) -> dict:
-        pass
-
-
-class PolygonIoClient(AbstractAPIClient):
-    URL = 'https://api.polygon.io/v3/'
-    
-    def get_api_token(self) -> str:
-        return settings.POLYGONIO_API_KEY
-    
-
-    async def tiker_list(self, tiker: AbstractTiker, session: ClientSession):
-        query = f'?apiKey={self.get_api_token()}'
-        request = self.URL + 'reference/tickers/' + query
-
-        async with session.get(request) as response:
-
-            if response.status == 404:
-                raise HTTPNotFound()
-            
-            return ujson.loads(await response.text())
-        
-
-    async def tiker_details(self, tiker: AbstractTiker, session: ClientSession):
-        query = f'?apiKey={self.get_api_token()}'
-        request = self.URL + 'reference/tickers/' + tiker.tiker + query
-
-        async with session.get(request) as response:
-            
-            if response.status == 404:
-                raise HTTPNotFound()
-            
-            return ujson.loads(await response.text())
-
-
-# async def main():
-#     cl1 = PolygonIoClient()
-# 
-#     async with ClientSession() as session:
-#         print(await cl1.tiker_details(Tiker('AAPL'), session))
-# 
-# asyncio.run(main())
+        return False

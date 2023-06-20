9cf6fa0009d25bf44ecb4c94f237271fef75be39
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-06-20 10:57:20 +0300

    Add default email template

--- api/settings.py
+++ api/settings.py
@@ -8,6 +8,8 @@ load_dotenv('.env')
 POLYGONIO_API_KEY = 'AVodlDZT9tcJBX3dOgc1VEhPhepXEKKu'
 DATA_API_CLIENT = PolygonIoClient
 
+DEFAULT_EMAIL_TEMPLATE = None
+
 DATABASE_URL = os.environ('DATABASE_URL')
 DATABASE_HOST = os.environ('DATABASE_HOST')
 DATABASE_PORT = os.environ('DATABASE_PORT')

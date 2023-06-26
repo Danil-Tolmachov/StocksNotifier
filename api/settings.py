b9fcb7924432b95f5cba43a5a35fa93a41462dd0
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-06-26 15:47:49 +0300

    Update settings

--- api/settings.py
+++ api/settings.py
@@ -1,18 +1,67 @@
 from dotenv import load_dotenv
 import os
 
-from services.checkers import PolygonIoClient
+from services.api_client import PolygonIoClient
+
+
+# Environment setup
+def env(var: str):
+    try:
+        return os.environ[var]
+    except KeyError:
+        return None
+
 
 load_dotenv('.env')
 
-POLYGONIO_API_KEY = 'AVodlDZT9tcJBX3dOgc1VEhPhepXEKKu'
-DATA_API_CLIENT = PolygonIoClient
+
+# Variables
+DATA_API_CLIENT = PolygonIoClient # AbstractAPIClient
 
 DEFAULT_EMAIL_TEMPLATE = None
 
-DATABASE_URL = os.environ('DATABASE_URL')
-DATABASE_HOST = os.environ('DATABASE_HOST')
-DATABASE_PORT = os.environ('DATABASE_PORT')
-DATABASE_NAME = os.environ('DATABASE_NAME')
-DATABASE_USER = os.environ('DATABASE_USER')
-DATABASE_PASSWORD = os.environ('DATABASE_PASSWORD')
+DATABASE_URL = env('DATABASE_URL')
+DATABASE_HOST = env('DATABASE_HOST')
+DATABASE_PORT = env('DATABASE_PORT')
+DATABASE_NAME = env('DATABASE_NAME')
+DATABASE_USER = env('DATABASE_USER')
+DATABASE_PASSWORD = env('DATABASE_PASSWORD')
+
+
+# Mongo Structure / Active classes
+#
+# WARNING: Sensitive settings
+# Users might loose their checkers if you delete/move or change it. 
+#
+checker_types = [
+    {
+        'id': 1,
+        'class': 'EverydayChecker',
+    },
+    {
+        'id': 2,
+        'class': 'GrowthChecker',
+    },
+    {
+        'id': 3,
+        'class': 'DropChecker',
+    },
+]
+
+delivery_types = [
+    {
+        'id': 1,
+        'class': 'EmailDelivery',
+    },
+]
+
+subscription_types = [
+    {
+        'id': 1,
+        'class': 'IndividualSubscription',
+    },
+    {
+        'id': 2,
+        'class': 'GroupSubscription',
+    },
+]

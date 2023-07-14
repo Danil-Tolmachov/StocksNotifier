5dfdc18f41a028612c8ffc1ce473f2c3909ccc27
Danil Tolmachov <danil.tolmachov45@gmail.com>
2023-07-14 14:25:09 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI
 import uvicorn
 
-import settings
 from routes.v1 import auth
+import settings
+
 
 api = FastAPI()
 
 api.include_router(auth.router)
 
-
-@api.get('/test')
-def test():
-    return 'success'
 
 
 if __name__ == '__main__':
